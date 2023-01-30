127ae9d1b2990fcc4b672bbe1f2c7c0aad215b76
handa-kun <Whitey.A@yandex.ru>
2023-01-31 01:45:03 +0300

    feat: added svg and settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Testing button</Htag>
-      <Button appearance='primary'>Subscribe</Button>
-      <Button appearance='ghost'>Subscribe</Button>
+      <Button appearance='primary' arrow='right'>Subscribe</Button>
+      <Button appearance='ghost' arrow='down'>Subscribe</Button>
     </>
   );
 }
