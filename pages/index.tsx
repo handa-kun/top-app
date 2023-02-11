66e5c94debee5db0b32c16e91a34feb788ad6572
handa-kun <Whitey.A@yandex.ru>
2023-02-11 14:34:20 +0300

    feat: added rating and another tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
-import { Button, Htag } from './../components';
+import { useState } from 'react';
+import { Button, Htag, Ptag, Rating, Tag } from './../components';
 
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
-      <Htag tag='h1'>Testing button</Htag>
-      <Button appearance='primary' arrow='right'>Subscribe</Button>
+      <Htag tag='h1'>Test</Htag>
+      <Button appearance='primary' arrow='right'>Send</Button>
       <Button appearance='ghost' arrow='down'>Subscribe</Button>
+      <Ptag size='s'>Test</Ptag>
+      <Ptag>Test</Ptag>
+      <Ptag size='l'>Test</Ptag>
+      <Tag size='m' color='red' href='#'>Send</Tag>
+      <Tag color='green' href='#'>Back</Tag>
+      <Tag color='primary' href='#'>Back</Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
 }
