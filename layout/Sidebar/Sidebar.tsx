ccc1900ede88b37ce0552e4833a82da826e83689
handa-kun <Whitey.A@yandex.ru>
2023-02-15 14:27:48 +0300

    feat: added logo

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,13 @@ import cn from 'classnames';
 import { Menu } from '../Menu/Menu';
 import styles from './Sidebar.module.css';
 import { SidebarProps } from './Sidebar.props';
+import Logo from '../logo.svg';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
     return (
-        <div {...props}>
+        <div {...props} className={cn(className, styles.sidebar)}>
+            <Logo className={styles.logo} />
+            <div>Search</div>
             <Menu />
         </div>
     );
