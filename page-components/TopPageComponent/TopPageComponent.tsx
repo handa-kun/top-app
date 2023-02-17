54166acb7df7cbe2052e25923b2056ca09fb0db0
handa-kun <Whitey.A@yandex.ru>
2023-02-17 22:42:22 +0300

    feat: added page-component and styles

---
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -0,0 +1,25 @@
+import { HhData, Htag, Tag } from '../../components';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+import styles from './TopPageComponent.module.css';
+import { TopPageComponentProps } from './TopPageComponent.props';
+
+
+export const TopPageComponent = ({ page, firstCategory, products }: TopPageComponentProps): JSX.Element => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
+                <Htag tag='h1'>{page.title}</Htag>
+                {products.length && <Tag color='grey' size='m'>{products.length}</Tag>}
+                <span>Soon</span>
+            </div>
+            <div>
+                {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+            </div>
+            <div className={styles.hhTitle}>
+                <Htag tag='h2'>Вакансии - {page.category}</Htag>
+                <Tag color='red' size='m'>hh.ru</Tag>
+            </div>
+            {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
+        </div>
+    );
+};
