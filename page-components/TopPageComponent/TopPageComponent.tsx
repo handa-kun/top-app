1f1f6a6c32373096ce72aca69174e8f6965f42b0
handa-kun <Whitey.A@yandex.ru>
2023-02-18 16:04:14 +0300

    feat: started on styles

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -2,6 +2,7 @@ import { HhData, Htag, Tag } from '../../components';
 import { TopLevelCategory } from '../../interfaces/page.interface';
 import styles from './TopPageComponent.module.css';
 import { TopPageComponentProps } from './TopPageComponent.props';
+import CheckIcon from './check.svg';
 
 
 export const TopPageComponent = ({ page, firstCategory, products }: TopPageComponentProps): JSX.Element => {
@@ -20,6 +21,16 @@ export const TopPageComponent = ({ page, firstCategory, products }: TopPageCompo
                 <Tag color='red' size='m'>hh.ru</Tag>
             </div>
             {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
+            <div>
+                <Htag tag='h2'>Преимущества</Htag>
+            </div>
+            <div>
+                <div>
+                    <CheckIcon />
+                </div>
+                <Htag tag='h3'>Title</Htag>
+                {products && products.map(p => (<p key={p._id}>{p.advantages}</p>))}
+            </div>
         </div>
     );
 };
